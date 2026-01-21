4b92c97876e1345adcb8936708f5f5eb5a8afce9
prtham24 <108997486+prtham24@users.noreply.github.com>
2026-01-21 11:19:52 +0530

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
 from PIL import Image
-from tkinter import Tk, filedialog
+from tkinter import Tk,filedialog
 A="@%#*+=-:. "
-
-
-def image_to_ascii(image_path,width):
+def resize(image,new_width=100):
+    width,height=image.size
+    aspect_ratio=height/width
+    new_height=int(new_width*aspect_ratio*0.55)
+    return image.resize((new_width,new_height))
+def greyscale(img):
+    return img.convert("L")
+def pixels_to_ascii(img):
+    pixels=img.getdata()
+    ascii_str=""
+    for pixel in pixels:
+        ascii_str+=A[pixel*len(A)//256]
+    return ascii_str
+def image_to_ascii(file_path,width):
     try:
-        image=Image.open(image_path)
+        image = Image.open(file_path)
     except Exception as e:
         print("Cannot open image",e)
         return
-    image=resize(image,width)
-    image=grayscale(image)
-    ascii_str=pixels_to_image(image)
-    width_size=image.width
+    img = Image.open(file_path)
+    img=resize(img,width)
+    img = greyscale(img)
+    ascii_str=pixels_to_ascii(img)
     ascii_img=""
-    for i in range(0,len(ascii_str),ascii_)
+    for i in range(0,len(ascii_str),width):
+        ascii_img+=ascii_str[i:i+width]+"\n"
+    print(ascii_img)
+def upload_file():
+    root=Tk()
+    root.withdraw()
+    file_path=filedialog.askopenfilename()
+    title="Select Image"
+    filetypes=[("Image Files","*png,*jpg,.jpeg")]
+    if file_path:
+        image_to_ascii(file_path, width=120)
+    else:
+        print("No file selected")
 
-
-image_to_ascii(file_path,width=120)
+upload_file()
